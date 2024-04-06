1b78e67de857eb7fa1c48a073df0fe94c449354b
Asa Masterson <asa.masterson@gmail.com>
2024-04-06 21:59:31 +0100

    Implament deeplinking

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 void main() => runApp(const MyApp());
+
+final _router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const MyApp(),
+    ),
+  ],
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
