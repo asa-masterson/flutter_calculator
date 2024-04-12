11bbe364db3d63aa984db00635391e4410a68781
Asa Masterson <asa.masterson@gmail.com>
2024-04-13 00:30:56 +0100

    PINK-3 Add assetlinks for android deeplinking verification

---
+++ functions/.well-known/assetlinks.json.js
@@ -0,0 +1,17 @@
+export default {
+  async fetch(request) {
+    const data = [
+      {
+        "relation": ["delegate_permission/common.handle_all_urls"],
+        "target": {
+          "namespace": "android_app",
+          "package_name": "com.pigsare.pink.calculator.calculator",
+          "sha256_cert_fingerprints":
+            ["B1:98:B9:4C:AE:12:06:6D:29:39:48:1B:AF:F1:C1:57:48:81:CB:FD:31:3B:EB:FC:35:2B:5D:E0:65:C9:E9:A5"]
+        }
+      }
+    ];
+
+    return Response.json(data);
+  },
+};
