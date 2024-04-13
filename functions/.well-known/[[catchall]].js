45fbb4713a22f4ab6da32a287c78f7f71bbb057e
Asa Masterson <asa.masterson@gmail.com>
2024-04-13 01:41:52 +0100

    PINK-3 Make sure data is only available via "assetlinks.json"

--- functions/.well-known/[[catchall]].js
+++ functions/.well-known/[[catchall]].js
@@ -1,14 +1,18 @@
 export function onRequest(context) {
-    const data = [
-      {
-        "relation": ["delegate_permission/common.handle_all_urls"],
-        "target": {
-          "namespace": "android_app",
-          "package_name": "com.pigsare.pink.calculator.calculator",
-          "sha256_cert_fingerprints":
-            ["B1:98:B9:4C:AE:12:06:6D:29:39:48:1B:AF:F1:C1:57:48:81:CB:FD:31:3B:EB:FC:35:2B:5D:E0:65:C9:E9:A5"]
-        }
-      }
-    ];
-    return Response.json(data);
+    if (context.params.catchall == "assetlinks.json") {
+        const data = [
+          {
+            "relation": ["delegate_permission/common.handle_all_urls"],
+            "target": {
+              "namespace": "android_app",
+              "package_name": "com.pigsare.pink.calculator.calculator",
+              "sha256_cert_fingerprints":
+                ["B1:98:B9:4C:AE:12:06:6D:29:39:48:1B:AF:F1:C1:57:48:81:CB:FD:31:3B:EB:FC:35:2B:5D:E0:65:C9:E9:A5"]
+            }
+          }
+        ];
+        return Response.json(data);
+    } else {
+        return new Response("")
+    }
 }
